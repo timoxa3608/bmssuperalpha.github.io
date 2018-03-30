e1359232a5bf3b060a2f1596783353955486bd8d
timoxa3608 <37663704+timoxa3608@users.noreply.github.com>
2018-03-30 23:19:22 +0300

    FILES

---
+++ offline.js
@@ -0,0 +1,33 @@
+﻿{
+	"version": 1522441129,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"images/fon-sheet0.png",
+		"images/zemla-sheet0.png",
+		"images/fewfd-sheet0.png",
+		"images/player-sheet0.png",
+		"images/telo-sheet0.png",
+		"images/oject-sheet0.png",
+		"images/weapon-sheet0.png",
+		"images/bordergorod-sheet0.png",
+		"images/bandicam_20180321_2-sheet0.png",
+		"images/bandicam_20180321_2-sheet1.png",
+		"images/праволево_шапки-sheet0.png",
+		"images/шапкаа_ui-sheet0.png",
+		"images/шапкаа_ui-sheet1.png",
+		"images/глаза_ui2-sheet0.png",
+		"images/border111-sheet0.png",
+		"images/weapon2-sheet0.png",
+		"images/fonnn-sheet0.png",
+		"images/fonn-sheet0.png",
+		"images/battle-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
